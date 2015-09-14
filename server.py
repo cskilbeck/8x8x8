3d8b3e099835e0c7ac5720c079ed3f56e562bcb1
cskilbeck <github@charlie.skilbeck.com>
2015-09-14 18:24:03 +0100

    no message

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
 #----------------------------------------------------------------------
 # TODO (chs): proper login/session thing
 # TODO (chs): bcrypt password
+# TODO (chs): parameter validation: min, max, minlength, maxlength, prepend, append, replace
+# TODO (chs): autogenerate REST docs from parameter check dictionaries
+# TODO (chs): make delete undoable for N days
+# DONE (chs): parameter validation and conditioning engine
+# DONE (chs): DRY the REST functions
 #----------------------------------------------------------------------
 
+import types
 import web
 from contextlib import closing
 import MySQLdb as mdb
 import MySQLdb.cursors
 import pprint
 import bcrypt
+import datetime
+import inspect
 import json
+import iso8601
+import unicodedata
 
 #----------------------------------------------------------------------
 
@@ -23,6 +33,7 @@ urls = (
     '/save', 'save',
     '/source', 'source',
     '/count', 'count',
+    '/delete', 'delete',
     '/list', 'list')
 
 #----------------------------------------------------------------------
@@ -64,204 +75,220 @@ def opendb():
 
 class favicon:
     def GET(self):
-        raise web.seeother("/static/favicon.ico")
+        raise web.seeother('/static/favicon.ico')
 
 #----------------------------------------------------------------------
 
-def paramcheck(params, argNames):
-    pprint.pprint(params)
-    pprint.pprint(argNames)
-    for i in argNames:
-        if not i in params:
-            raise ValueError
+class RequestHandler:
+    def mainHandler(self, handler):
+        try:
+            with closing(opendb()) as db:
+                with closing(db.cursor()) as cur:
+                    return getJSON(handler(db, cur, data = web.input()))
+
+        except ValueError, e:
+            pprint.pprint(e)
+            raise web.HTTPError('404 ' + str(e))
+
+        except KeyError, e:
+            pprint.pprint(e)
+            raise web.HTTPError('404 ' + str(e))
+
+        except mdb.Error, e:
+            pprint.pprint(e)
+            raise web.HTTPError('500 Database problem')
+
+        except web.HTTPError, e:
+            raise
+
+        except Exception, e:
+            pprint.pprint(e)
+            raise web.HTTPError('500 Unknown error')
+
+class Post(RequestHandler):
+    def POST(self):
+        try:
+            return self.mainHandler(self.handlePost)
+        except KeyError:
+            raise web.HTTPError('405 Invalid Method')
+
+class Get(RequestHandler):
+    def GET(self):
+        try:
+            return self.mainHandler(self.handleGet)
+        except KeyError:
+            raise web.HTTPError('405 Invalid Method')
+
+#----------------------------------------------------------------------
+
+def checked(paramspec):
+    def wrapper(func):
+        def new_fun(*args, **kwargs):
+            result = {}
+            data = kwargs.get('data', {})
+            for name in paramspec:
+                default = paramspec[name]
+                deftype = type(default)
+                val = data.get(name, None)
+                if val is not None:
+                    val = unicodedata.normalize('NFKD', val).encode('ascii','ignore')
+
+                #! use a tuple for enum !
+
+                # type specified, so it's a required parameter of deftype - ValueError if it's not there int(none)
+                if deftype == type:
+                    if val is None:
+                        raise ValueError('Missing parameter %s' % (name,))
+                    try:
+                        val = default(val)
+                    except TypeError:
+                        raise ValueError('%s cannot be cast to %s' % (name, type(default).__name__))
+
+                # dictionary {'type': int, 'default': 0, ... }
+                elif deftype == dict:
+                    defval = default.get('default', None)
+                    deftype = default.get('type', None)
+                    if deftype is None:
+                        if defval is None:
+                            raise ValueError('Invalid paramspec')   # must specify default or type or both
+                        deftype = type(defval)
+                    val = deftype(defval) if val is None else deftype(val)
+
+                    # TODO (chs): handle min and max for int, str, float and datetime etc
+
+                # default value specified [int|float|str]
+                elif deftype in [int, float, str]:
+                    val = default if val is None else deftype(val)
+
+                # default datetime specified
+                elif deftype == datetime.datetime:
+                    val = default if val is None else iso8601.parse_date(val)
+
+                # got a val, after all that?
+                if val is None:
+                    raise KeyError('Parameter %s is missing (expected: %s)' % (name, default.__name__))
+                else:
+                    result[name] = val
+            return func(*args, data = result)
+        return new_fun
+    return wrapper
+
+#----------------------------------------------------------------------
+# list
+
+class list(Get):
+    @checked({
+        'user_id': int,
+        'search': '',
+        'length': { 'default': 20, 'type': int, 'max': 100, 'min': 1 },
+        'offset': { 'default': 0, 'min': 0 }
+        })
+    def handleGet(self, db, cur, data):
+        data['search'] = '%' + data['search'].replace('*', '%').replace('.', '_') + '%'
+        cur.execute('''SELECT game_id, games.user_id, game_title, game_lastsaved, game_created, user_username
+                        FROM games INNER JOIN users ON users.user_id = games.user_id
+                        WHERE games.user_id = %(user_id)s AND game_title LIKE %(search)s
+                        ORDER BY game_lastsaved, game_created DESC
+                        LIMIT %(length)s OFFSET %(offset)s'''
+                    , data)
+        return { 'count': cur.rowcount, 'games': cur.fetchall() }
 
 #----------------------------------------------------------------------
 # count
-# GET: (used_id, search)
 
-class count:
-    def GET(self):
-        data = web.input()
-        result = {"status": "ok"}
-        try:
-            params = {
-                'user_id': int(data.get('user_id', 0)),
-                'search': '%' + data.get('search', '').replace('*', '%').replace('.', '_') + '%'
-            }
-            with closing(opendb()) as db:
-                with closing(db.cursor()) as cur:
-                    cur.execute("""SELECT COUNT(*) AS count 
-                                    FROM games 
-                                    WHERE user_id = %(user_id)s AND game_title LIKE %(search)s"""
-                                , params)
-                    result['count'] = cur.fetchone()['count']
-        except ValueError:
-            web.ctx.status = '400 Invalid parameters'
-            result['status'] = "error"
-        except mdb.Error, e:
-            web.ctx.status = '500 Database problem'
-            result['status'] = "error"
-        return getJSON(result)
+class count(Get):
+    @checked({ 'user_id': int, 'search': '' })
+    def handleGet(self, db, cur, data):
+        data['search'] = '%' + data['search'].replace('*', '%').replace('.', '_') + '%'
+        cur.execute('SELECT COUNT(*) AS count FROM games WHERE user_id = %(user_id)s AND game_title LIKE %(search)s', data)
+        return cur.fetchone()
 
 #----------------------------------------------------------------------
 # source
 
-class source:
-    def GET(self):
-        data = web.input()
-        result = {"status": "ok"}
-        try:
-            paramcheck(data, ['game_id'])
-            with closing(opendb()) as db:
-                with closing(db.cursor()) as cur:
-                    cur.execute("SELECT game_source FROM GAMES WHERE game_id=%s", (int(data['game_id']),))
-                    if cur.rowcount == 0:
-                        web.ctx.status = '400 game not found'
-                        result = {"status": "error"}
-                    else:
-                        row = cur.fetchone()
-                        result['source'] = row['game_source']
-        except ValueError:
-            web.ctx.status = '400 Invalid parameters'
-            result['status'] = "error"
-        except mdb.Error, e:
-            web.ctx.status = '500 Database problem'
-            result['status'] = "error"
-        return getJSON(result)
-
-#----------------------------------------------------------------------
-# list
-# GET: (user_id) [length, offset, datebegin, dateend, search, orderby=[rating,created,saved,name]]
-
-class list:
-    def GET(self):
-        data = web.input()
-        pprint.pprint(data)
-        result = {"status": "ok"}
-        params = {
-            'user_id': int(data.get('user_id', 0)),
-            'search': '%' + data.get('search', '').replace('*', '%').replace('.', '_') + '%',
-            'length': min(100, int(data.get('length', 20))),
-            'offset': min(100, int(data.get('offset', 0)))
-        }
-        try:
-            with closing(opendb()) as db:
-                with closing(db.cursor()) as cur:
-                    cur.execute("""SELECT game_id, games.user_id, game_title, game_lastsaved, game_created, user_username
-                                    FROM games
-                                        INNER JOIN users ON users.user_id = games.user_id 
-                                    WHERE games.user_id = %(user_id)s AND game_title LIKE %(search)s
-                                    ORDER BY game_lastsaved, game_created DESC
-                                    LIMIT %(length)s OFFSET %(offset)s"""
-                                , params)
-                    result['games'] = cur.fetchall()
-        except mdb.Error, e:
-            pprint.pprint(e)
-            web.ctx.status = '500 Database problem'
-            result["status"] = "error"
-        return getJSON(result)
+class source(Get):
+    @checked({ 'game_id': int })
+    def handleGet(self, db, cur, data):
+        cur.execute('SELECT game_source as source, game_title FROM GAMES WHERE game_id=%(game_id)s', data)
+        if cur.rowcount == 1:
+            return cur.fetchone()
+        raise web.HTTPError('404 Game not found')
 
 #----------------------------------------------------------------------
 # save
-# POST: {'user_id': 1, 'source': 'sourcecode', 'name': 'snake' }
 
-class save:
-    def POST(self):
-        data = web.input()
-        result = {"status": "ok"}
-        try:
-            paramcheck(data, ['user_id', 'source', 'name'])
-            with closing(opendb()) as db:
-                with closing(db.cursor()) as cur:
-                    cur.execute("SELECT * FROM users WHERE user_id =%(user_id)s", data)
-                    if cur.fetchone() is None:
-                        web.ctx.status = '401 Invalid user_id'
-                    else:
-                        cur.execute("""INSERT INTO games (user_id, game_created, game_lastsaved, game_source, game_title)
-                                        VALUES (%(user_id)s, NOW(), NOW(), %(source)s, %(name)s)
-                                        ON DUPLICATE KEY UPDATE game_source = VALUES(game_source), game_lastsaved = VALUES(game_lastsaved)"""
-                                    , data)
-                        result['saved'] = cur.rowcount
-        except ValueError:
-            web.ctx.status = '404 Invalid parameter'
-            result["status"] = "error"
-        except mdb.Error, e:
-            result["status"] = "error"
-            web.ctx.status = '500 Database problem'
-        return getJSON(result)
+class save(Post):
+    @checked({ 'user_id': int, 'source': str, 'name': str })
+    def handlePost(self, db, cur, data):
+        cur.execute('SELECT * FROM users WHERE user_id =%(user_id)s', data)
+        if cur.fetchone() is None:
+            raise web.HTTPError('401 Invalid user_id')
+        else:
+            cur.execute('''INSERT INTO games (user_id, game_created, game_lastsaved, game_source, game_title)
+                            VALUES (%(user_id)s, NOW(), NOW(), %(source)s, %(name)s)
+                            ON DUPLICATE KEY UPDATE game_source = VALUES(game_source), game_lastsaved = VALUES(game_lastsaved)''' , data)
+            return { 'saved': cur.rowcount }
 
+#----------------------------------------------------------------------
+# delete
+
+class delete(Post):
+    @checked({ 'user_id': int, 'game_id': int })
+    def handlePost(self, db, cur, data):
+        cur.execute('DELETE FROM games WHERE game_id = %(game_id)s AND user_id = %(user_id)s', data)
+        if cur.rowcount == 0:
+            raise web.HTTPError('404 Game not found')
+        return { 'deleted': cur.rowcount }
 
 #----------------------------------------------------------------------
 # register
-# POST: { 'email': 'charlie@skilbeck.com', 'password': 'henry1', 'username': 'cskilbeck' }
 
-class register:
-    def parameters(self):
-        return ['username', 'email', 'password']
-
-    def POST(self):
-        data = web.input()
-        result = { 'status': 'error' }
-        user_id = 0
-        try:
-            paramcheck(data, ['username', 'email', 'password'])
-            with closing(opendb()) as db:
-                with closing(db.cursor()) as cur:
-                    cur.execute("SELECT COUNT(*) AS count FROM users WHERE user_email=%(email)s", data)
-                    if cur.fetchone()['count'] != 0:
-                        result['message'] = 'Email already taken'
-                    else:
-                        cur.execute("SELECT COUNT(*) AS count FROM users WHERE user_name=%(username)s", data)
-                        if cur.fetchone()['count'] != 0:
-                            result['message'] = 'Username already taken'
-                        else:
-                            cur.execute("INSERT INTO users (user_email, user_password, user_name, user_created) VALUES (%(email)s, %(password)s, %(username)s, NOW())", data)
-                            user_id = cur.lastrowid
-                            session().user_id = user_id
-                            result['status'] = 'ok'
-                            result['user_id'] = user_id
-                            result['user_username'] = data['username']
-        except ValueError:
-            web.ctx.status = '404 Invalid parameter'
-        except mdb.Error, e:
-            web.ctx.status = '500 Database problem'
-        return getJSON(result)
+class register(Post):
+    @checked({ 'email': str, 'password': str, 'username': str })
+    def handlePost(self, db, cur, data):
+        result = {}
+        cur.execute('SELECT COUNT(*) AS count FROM users WHERE user_email=%(email)s', data)
+        if cur.fetchone()['count'] != 0:
+            raise web.HTTPError('401 Email already taken')
+        else:
+            cur.execute('SELECT COUNT(*) AS count FROM users WHERE user_username=%(username)s', data)
+            if cur.fetchone()['count'] != 0:
+                raise web.HTTPError('401 Username already taken')
+            else:
+                cur.execute('''INSERT INTO users (user_email, user_password, user_username, user_created)
+                                VALUES (%(email)s, %(password)s, %(username)s, NOW())''', data)
+                if cur.rowcount > 0:
+                    user_id = cur.lastrowid
+                    session().user_id = user_id
+                    result['user_id'] = user_id
+                    result['user_username'] = data['username']
+                else:
+                    raise web.HTTPError("401 Can't create account")
+        return result
 
 #----------------------------------------------------------------------
 # login
-# POST: { 'email': 'charlie@skilbeck.com', 'password': 'henry1' }
 
-class login:
-    def POST(self):
-        data = web.input()
-        result = {"status": "error"}
-        user_id = 0
-        try:
-            paramcheck(data, ['email', 'password'])
-            with closing(opendb()) as db:
-                with closing(db.cursor()) as cur:
-                    cur.execute("SELECT * FROM users WHERE user_email =%(email)s", data)
-                    if cur.rowcount > 0:
-                        row = cur.fetchone()
-                        if row['user_password'] == data['password']:
-                            user_id = row['user_id']
-                            session().user_id = user_id
-                            result['status'] = "ok"
-                            result['user_id'] = user_id
-                            result['user_username'] = row['user_username']
-                        else:
-                            web.ctx.status = '401 Incorrect username or password'
-                    else:
-                        web.ctx.status = '401 Incorrect username or password'
-        except ValueError:
-            web.ctx.status = '404 Invalid parameter'
-        except mdb.Error, e:
-            web.ctx.status = '500 Database problem'
-        return getJSON(result)
+class login(Post):
+    @checked({ 'email': str, 'password': str })
+    def handlePost(self, db, cur, data):
+        result = {}
+        cur.execute('SELECT * FROM users WHERE user_email =%(email)s', data)
+        if cur.rowcount > 0:
+            row = cur.fetchone()
+            if row['user_password'] == data['password']:
+                user_id = row['user_id']
+                session().user_id = user_id
+                result['user_id'] = user_id
+                result['user_username'] = row['user_username']
+            else:
+                raise web.HTTPError('401 Incorrect username or password')
+        else:
+            raise web.HTTPError('401 Incorrect username or password')
+        return result
 
 #----------------------------------------------------------------------
 # index
-# GET: /
 
 class index:
     def GET(self):
@@ -269,7 +296,7 @@ class index:
         if token is not None:
             web.setcookie('session', session, 60 * 24 * 30)
         else:
-            web.setcookie('session', "", -1)
+            web.setcookie('session', '', -1)
         return render.index()
 
 #----------------------------------------------------------------------
